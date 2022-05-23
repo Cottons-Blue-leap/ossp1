a752819a0bcd3560ef1522e2b782af442fe7ea1d
Cottons-Blue-leap <wlsxorb1@naver.com>
2022-05-24 05:26:52 +0900

    ADD Sex my_info.py

--- my_info.py
+++ my_info.py
@@ -3,10 +3,12 @@ def my_info():
     name = input("Write your name: ")
     id = input("Write your Student ID: ")
     major = input("Write your Major: ")
+    sex = input("Write your Sex: ")
 
     print("\n<PRINT>")
     print(f"NAME: {name}")
     print(f"ID: {id}")
     print(f"MAJOR: {major}")
+    print(f"SEX: {sex}")
 
 my_info()

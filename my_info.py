a48d079c60909d7aef0d85c9b5000b4c00b63fc9
Cottons-Blue-leap <wlsxorb1@naver.com>
2022-05-24 04:56:36 +0900

    ADD Major my_info.py

--- my_info.py
+++ my_info.py
@@ -2,9 +2,11 @@ def my_info():
 
     name = input("Write your name: ")
     id = input("Write your Student ID: ")
+    major = input("Write your Major: ")
 
     print("\n<PRINT>")
     print(f"NAME: {name}")
     print(f"ID: {id}")
+    print(f"MAJOR: {major}")
 
 my_info()
